04d0f8e0788408adb136d51588ab4184ae7e7b52
DMelnikov <dmitriymelnikov@mail.ru>
2022-06-30 21:04:49 +0300

    Task number 1 complete

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b;
+
+Console.Write("Input number a:  ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number b:  ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+Console.WriteLine(b);
+}
+
+
